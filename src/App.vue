6ad008c908aca2903c2d9c0196c065ef34bb60f0
Jonathan Almeida <slipjfa@gmail.com>
2022-12-24 01:41:35 -0300

    tic tac toe

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,89 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed } from 'vue'
+
+const player = ref('X');
+
+const board = ref([
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', ''],
+]);
+
+const CalculateWinner = (board) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a];
+    }
+  }
+  return null;
+}
+
+const winner = computed(() => CalculateWinner(board.value.flat()));
+
+const MakeMove = (x, y)  => {
+  if (winner.value) return;
+
+  if (board.value[x][y]) return;
+
+  board.value[x][y] = player.value;
+
+  player.value = player.value === 'X' ? 'O' : 'X';
+}
+
+const ResetGame = () => {
+  board.value = [
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', ''],
+  ];
+  player.value = 'X';
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main class="pt-8 text-center dark:bg-gray-800 min-h-screen">
+    <h1 class="mb-8 text-3xl font-bold uppercase">Tic Tac Toe</h1>
+    <h3 class="text-xl mb-4">Player <span class="text-green-500">{{ player }}</span> turn</h3>
+    <div class="flex flex-col items-center mb-8">
+      <div 
+        v-for="(row, x) in board"
+        :key="x"
+        class="flex"
+      >
+        <div 
+          v-for="(cell, y) in row"
+          :key="y"
+          @click="MakeMove(x, y)"
+          :class="`border border-white w-24 h-24 hover:bg-gray-700 flex items-center 
+            justify-center material-icons-outlined text-4xl cursor-pointer 
+            ${cell === 'X' ? 'text-red-500' : 'text-blue-400'}`"
+        >
+					{{ cell === 'X' ? 'X' : cell === 'O' ? 'O' : '' }}
+        </div>
+      </div>
+    </div>
+    <h2 v-if="winner" class="text-4xl font-bold mb-8">Player <span class="text-purple-600">{{ winner }}</span> wins!</h2>
+    <button @click="ResetGame" 
+      class="px-4 py-2 bg-red-500 
+      rounded uppercase font-bold 
+      hover:bg-red-600 duration-300"
+    >
+      Reset Game 
+    </button>
+  </main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+
 </style>
