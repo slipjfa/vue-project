f0026244a459af211e91dde2126705d9222ba82f
Jonathan Almeida <2251555+slipjfa@users.noreply.github.com>
2023-01-01 16:30:04 -0300

    card component

---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import { defineProps } from 'vue';
+
+  const { quiz } = defineProps(['quiz']);
+  console.log(quiz)
+</script>
+
+<template>
+   <div class="card rounded-sm mb-9 mr-5 cursor-pointer shadow-card">
+        <img class="img w-full h-48 w-[310px] m-0" :src="quiz.img" alt="">
+        <div class="card-text pt-0 pr-1">
+          <h2 class="text-bold">{{ quiz.name }}</h2>
+          <p>{{ quiz.questions.length }} questions</p>
+        </div>
+      </div>
+</template>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'card': '1px 1px 10px rgba(0, 0, 0, 1)',
+      }
+    },
   },
   plugins: [],
 }
